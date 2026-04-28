13aa120a77fa52794b055a24c2dad63eafaea328
Yakayna <namnguyenthe2006@gmail.com>
2026-04-29 00:51:56 +0700

    Fix: output_file set before hash was computed, causing rclone upload failure

--- uploadROM.sh
+++ uploadROM.sh
@@ -36,9 +36,9 @@ elif [[ $BRAND == "RealmeUI" ]]; then
   uploaddir="RealmeUI"
 fi
 
-output_file="out/${NTBUILD}_${VERSION}_${DEVICE_MODEL}_OS${BASE_BUILD_ID}_${hash}_${status}.zip"
 hash=$(md5sum out/${NTBUILD}_${DEVICE_MODEL}_${ANDROID_VER}_OS${BASE_BUILD_ID}.zip |head -c 5)
 mv out/${NTBUILD}_${DEVICE_MODEL}_${ANDROID_VER}_OS${BASE_BUILD_ID}.zip out/${NTBUILD}_${VERSION}_${DEVICE_MODEL}_OS${BASE_BUILD_ID}_${hash}_${status}.zip
+output_file="out/${NTBUILD}_${VERSION}_${DEVICE_MODEL}_OS${BASE_BUILD_ID}_${hash}_${status}.zip"
 echo "[SCRIPT] - Output: "
 echo "$(pwd)/out/${NTBUILD}_${VERSION}_${DEVICE_MODEL}_OS${BASE_BUILD_ID}_${hash}_${status}.zip"
 
